32aae6ea06a36e9f0320dfd7bf9c7adb3e2e6804
Igor <Sketch_90@mail.ru>
2024-09-30 21:05:14 +0300

    graddle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("javax.servlet:javax.servlet-api:4.0.1")
+    implementation("com.github.stephenc.jcip:jcip-annotations:1.0-1")
+
 }
 
 tasks.test {
