36cab0ebf0275aec884f756e54ee2371424b2e68
Igor <Sketch_90@mail.ru>
2024-12-15 12:56:46 +0300

    VisualComponent

---
+++ concurrency-taxi-tracker/src/main/java/org/example/visualcomponent/VisualComponent.java
@@ -0,0 +1,34 @@
+package org.example.visualcomponent;
+
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * <b>VisualComponent</b>
+ * <br><br>
+ * Delegating thread safety to multiple underlying state variables
+ */
+public class VisualComponent {
+
+    private final List<KeyListener> keyListeners = new CopyOnWriteArrayList<>();
+
+    private final List<MouseListener> mouseListeners = new CopyOnWriteArrayList<>();
+
+    public void addKeyListener(KeyListener keyListener) {
+        keyListeners.add(keyListener);
+    }
+
+    public void addMouseListener(MouseListener listener) {
+        mouseListeners.add(listener);
+    }
+
+    public void removeKeyListener(KeyListener listener) {
+        keyListeners.remove(listener);
+    }
+
+    public void removeMouseListener(MouseListener listener) {
+        mouseListeners.remove(listener);
+    }
+}
